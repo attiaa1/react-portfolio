6ea3b3b138ab40169a2b292b0365de0673ed96a3
Adam Attia <adduma.attia@gmail.com>
2024-08-09 20:55:42 -0400

    first page 3/4 done, todo: phone support for different chunks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useRef, useEffect, useState } from "react";
 import Chunk from "./components/Chunk";
+import Home from "./components/Home";
+import Home_BL from "./components/Home_BL";
+import Home_TR from "./components/Home_TR";
 import "./App.css";
 
 const Section = ({ children, className }) => {
@@ -43,10 +46,16 @@ const App = () => {
   return (
     <div className="wrapper">
       <Section className="page1">
-        <Chunk direction="north">North</Chunk>
+        <Chunk direction="north">
+        <Home></Home>
+        </Chunk>
         <Chunk direction="south">South</Chunk>
-        <Chunk direction="east">East</Chunk>
-        <Chunk direction="west">West</Chunk>
+        <Chunk direction="east">
+          <Home_TR></Home_TR>
+        </Chunk>
+        <Chunk direction="west">
+        <Home_BL></Home_BL>
+        </Chunk>
       </Section>
       <Section className="page2">
         <Chunk direction="north">North</Chunk>
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css'
+
+const Home = () => {
+  return (
+      <div className='blurb'>
+        <h1> Who am I?</h1>
+        <p>
+        My name is Adam. I'm a full-stack developer.
+        If you're looking for my resume, you can find it <a style={{color: "amber"}}href="https://adamattia.me/resume"> here</a>.
+        </p> 
+      </div>
+  );
+};
+
+export default Home;
