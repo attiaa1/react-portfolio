dc6b3d30a4abf1f247b86e0c6b2cc35e56e3ab1a
Adam Attia <adduma.attia@gmail.com>
2024-08-17 23:17:17 -0400

    errors deploying site on port, checking out dev branch

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import { resolve } from 'path';
 const { get } = server.router;
 
 // Serve static files from the 'dist' directory
-server({ public: resolve('dist') }, [
+server({ 
+  public: resolve('dist'),
+  port: 3000 // Specify the port here
+}, [
   get('/', ctx => ctx.res.sendFile(resolve('dist/index.html')))
 ]).then(ctx => {
   console.log(`Server running at http://localhost:${ctx.options.port}`);
