791152d54de369ba2fda7d65e5866e62f5a24655
Adam Attia <adduma.attia@gmail.com>
2025-05-22 00:06:21 -0400

    feat: add work experience 2 and revise work experience 1 styling

--- src/components/FullPageComponent.jsx
+++ src/components/FullPageComponent.jsx
@@ -2,46 +2,44 @@ import React from 'react';
 import ReactFullpage from '@fullpage/react-fullpage';
 import Hero from './Hero';
 import About from './About';
-import Project1 from './Project1';
-import Project2 from './Project2';
-import Project3 from './Project3';
 import Work1 from './Work1';
+import Work2 from './Work2';
 import Hobbies from './Hobbies';
 import '../App.css'
 
 const FullPageComponent = () => (
-  <ReactFullpage
-    // Fullpage.js options
-    licenseKey={'YOUR_KEY_HERE'}
-    scrollingSpeed={700} /* Options here */
-    loopBottom={'True'}
-    navigation={'true'}
+    <ReactFullpage
+        // Fullpage.js options
+        licenseKey={'YOUR_KEY_HERE'}
+        scrollingSpeed={700} /* Options here */
+        loopBottom={'True'}
+        navigation={'true'}
 
-    render={({ state, fullpageApi }) => {
-      return (
-        <ReactFullpage.Wrapper>
-          <div id='home' className="section">
-            <Hero />
-          </div>
-          <div id='about' className="section">
-            <About />
-          </div>
-          <div id='projects' className="section">
-            <Work1 />
-          </div>
-          <div id='projects' className="section">
-            <h1>Hello World 2!</h1>
-          </div>
-          <div id='projects' className="section">
-            <h1>Hello World 3!</h1>
-          </div>
-          <div id='experience' className="section">
-            <Hobbies />
-          </div>
-        </ReactFullpage.Wrapper>
-      );
-    }}
-  />
+        render={({ state, fullpageApi }) => {
+            return (
+                <ReactFullpage.Wrapper>
+                    <div id='home' className="section">
+                        <Hero />
+                    </div>
+                    <div id='about' className="section">
+                        <About />
+                    </div>
+                    <div id='projects' className="section">
+                        <Work1 />
+                    </div>
+                    <div id='projects' className="section">
+                        <Work2 />
+                    </div>
+                    <div id='projects' className="section">
+                        <h1>Hello World 3!</h1>
+                    </div>
+                    <div id='experience' className="section">
+                        <Hobbies />
+                    </div>
+                </ReactFullpage.Wrapper>
+            );
+        }}
+    />
 );
 
 export default FullPageComponent;
